1a21c4addbeb1f8669ec2ed29425bdc82cce305a
ouahid <ouahid.khelifi@gmail.com>
2021-08-23 13:04:43 +0200

    feat: remove feature flags scope

--- src/main/java/io/gravitee/platform/repository/api/Scope.java
+++ src/main/java/io/gravitee/platform/repository/api/Scope.java
@@ -30,8 +30,7 @@ public enum Scope {
     RATE_LIMIT("ratelimit"),
     ANALYTICS("analytics"),
     CACHE("cache"),
-    KEY_VALUE("keyvalue"),
-    FEATURE_FLAGS("featureflags");
+    KEY_VALUE("keyvalue");
 
     String name;
 
